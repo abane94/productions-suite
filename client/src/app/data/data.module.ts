7e1a0975eaa1ab8ff0dcc5c817bf04c0a7452124
Aris Husanu <abane94@gmail.com>
2021-05-30 14:36:47 -0400

    creates new data module for service, and materialService

---
+++ client/src/app/data/data.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialService } from './material.service';
+
+
+
+@NgModule({
+  declarations: [],
+  providers: [
+    MaterialService
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DataModule { }
--- client/src/app/materials/materials-list-page/materials-list-page.component.ts
+++ client/src/app/materials/materials-list-page/materials-list-page.component.ts
@@ -1,14 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MaterialService } from 'src/app/data/material.service';
 import { NestedDisplayFields } from 'src/app/forms/user-defined-form-data-display/user-defined-form-data-display.component';
 import { FormDefinition } from 'src/app/forms/user-defined-form-viewer/user-defined-form-viewer.component';
 import MaterialFormDefinition from '../material-form-definition';
-
-interface User {
-  id: number;
-  name: string;
-  creation: Date;
-  color: string;
-}
 
 @Component({
   selector: 'app-materials-list-page',
@@ -28,197 +22,12 @@ export class MaterialsListPageComponent implements OnInit {
   };
 
   formDef: FormDefinition = MaterialFormDefinition([], []);
-  materials: any[] = [
-    {
-      id: 1,
-      name: 'TShirt',
-      description: 'A simple tshirt',
-      'category': {value: 'clothing', display: 'clothing' },
-      'baseCost': 1.45,
-      'supplier': 'gilden',
-      'supplierItemUrl': 'dsagfds',
-      'options': {
-        'items': [
-          {
-            'name': 'Size',
-            'selections': {
-              'items': [
-                {
-                  'value': 'M',
-                  'display': 'M',
-                  'img': '',
-                  'priceAdjustment': 0
-                },
-                {
-                  'value': 'S',
-                  'display': 'S',
-                  'img': '',
-                  'priceAdjustment': 0
-                },
-                {
-                  'value': 'XXL',
-                  'display': 'XXL',
-                  'img': '',
-                  'priceAdjustment': 0.1
-                }
-              ]
-            }
-          },
-          {
-            'name': 'Color',
-            'selections': {
-              'items': [
-                {
-                  'value': 'BLUE',
-                  'display': 'BLUE',
-                  'img': '',
-                  'priceAdjustment': 0
-                },
-                {
-                  'value': 'RED',
-                  'display': 'RED',
-                  'img': '',
-                  'priceAdjustment': 0
-                },
-                {
-                  'value': 'TIE DYE',
-                  'display': 'TIE',
-                  'img': 'DYE',
-                  'priceAdjustment': 0.15
-                }
-              ]
-            }
-          }
-        ]
-      }
-    },
-    {
-      id: 2,
-      name: 'Long Sleeve',
-      description: 'A simple tshirt',
-      'category': {value: 'clothing', display: 'clothing' },
-      'baseCost': 1.45,
-      'supplier': 'gilden',
-      'supplierItemUrl': 'dsagfds',
-      'options': {
-        'items': [
-          {
-            'name': 'Size',
-            'selections': {
-              'items': [
-                {
-                  'value': 'M',
-                  'display': 'M',
-                  'img': '',
-                  'priceAdjustment': 0
-                },
-                {
-                  'value': 'S',
-                  'display': 'S',
-                  'img': '',
-                  'priceAdjustment': 0
-                },
-                {
-                  'value': 'XXL',
-                  'display': 'XXL',
-                  'img': '',
-                  'priceAdjustment': 0.1
-                }
-              ]
-            }
-          },
-          {
-            'name': 'Color',
-            'selections': {
-              'items': [
-                {
-                  'value': 'BLUE',
-                  'display': 'BLUE',
-                  'img': '',
-                  'priceAdjustment': 0
-                },
-                {
-                  'value': 'RED',
-                  'display': 'RED',
-                  'img': '',
-                  'priceAdjustment': 0
-                },
-                {
-                  'value': 'TIE DYE',
-                  'display': 'TIE',
-                  'img': 'DYE',
-                  'priceAdjustment': 0.15
-                }
-              ]
-            }
-          }
-        ]
-      }
-    },
-    {
-      id: 3,
-      name: 'Hoodie',
-      description: 'A simple tshirt',
-      'category': {value: 'clothing', display: 'clothing' },
-      'baseCost': 1.45,
-      'supplier': 'gilden',
-      'supplierItemUrl': 'dsagfds',
-      'options': {
-        'items': [
-          {
-            'name': 'Size',
-            'selections': {
-              'items': [
-                {
-                  'value': 'M',
-                  'display': 'M',
-                  'img': '',
-                  'priceAdjustment': 0
-                },
-                {
-                  'value': 'S',
-                  'display': 'S',
-                  'img': '',
-                  'priceAdjustment': 0
-                },
-                {
-                  'value': 'XXL',
-                  'display': 'XXL',
-                  'img': '',
-                  'priceAdjustment': 0.1
-                }
-              ]
-            }
-          },
-          {
-            'name': 'Color',
-            'selections': {
-              'items': [
-                {
-                  'value': 'BLUE',
-                  'display': 'BLUE',
-                  'img': '',
-                  'priceAdjustment': 0
-                },
-                {
-                  'value': 'RED',
-                  'display': 'RED',
-                  'img': '',
-                  'priceAdjustment': 0
-                },
-                {
-                  'value': 'TIE DYE',
-                  'display': 'TIE',
-                  'img': 'DYE',
-                  'priceAdjustment': 0.15
-                }
-              ]
-            }
-          }
-        ]
-      }
-    }
-  ];
+  materials: any[] = [];
+
+  selected!: any[];
+  constructor(private materialService: MaterialService) {
+    this.materialService.getMaterials().then(materials => this.materials = materials);
+  }
 
   onSave($event: any) {
     console.log('Saved!');
@@ -237,9 +46,6 @@ export class MaterialsListPageComponent implements OnInit {
       // this.materials[idx] = $event;  // TODO: fix angular error when saving, maybe needs the trackby function in ngFor
     }
   }
-
-  selected!: User[];
-  constructor() { }
 
   ngOnInit(): void {
   }
